13a6706b8dabb161c487d3c948f786d9222f4904
nixinfei <nxf@dxyer.com>
2022-04-19 16:46:13 +0800

    support build xcframework

--- makeXCFramework.sh
+++ makeXCFramework.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
 FRAMEWORK_NAME="FBSnapshotTestCase"
-IOS_SCHMEM="FBSnapshotTestCase-iOS"
-TV_SCHEME="FBSnapshotTestCase-tvOS"
 
 IOS_ARCHIVE_PATH="./build/archives/ios.xcarchive"
 IOS_SIMULATOR_ARCHIVE_PATH="./build/archives/ios_sim.xcarchive"
 
-xcodebuild archive -scheme "FBSnapshotTestCase iOS" -archivePath ${IOS_ARCHIVE_PATH} -sdk iphoneos SKIP_INSTALL=NO
-xcodebuild archive -scheme "FBSnapshotTestCase iOS" -archivePath ${IOS_SIMULATOR_ARCHIVE_PATH} -sdk iphonesimulator SKIP_INSTALL=NO
+xcodebuild archive -scheme "FBSnapshotTestCase iOS" -archivePath ${IOS_ARCHIVE_PATH} -sdk iphoneos SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+xcodebuild archive -scheme "FBSnapshotTestCase iOS" -archivePath ${IOS_SIMULATOR_ARCHIVE_PATH} -sdk iphonesimulator SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
 xcodebuild -create-xcframework \
   -framework ${IOS_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework \
   -framework ${IOS_SIMULATOR_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework \
   -output "./build/${FRAMEWORK_NAME}.xcframework"
+
+find ./build/ -name "*.swiftinterface" -exec sed -i -e 's/FBSnapshotTestCase\.//g' {} \;
+find ./build/ -name "*.swiftinterface-e" -exec rm {} \;
